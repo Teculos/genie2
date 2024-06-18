e324b2bc08f7dd5bfcb6c472d5a8dc47dfd55bbc
Tony <tonyculos@gmail.com>
2024-06-18 13:33:53 -0400

    init files

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(
       name='genie',
       version='0.0.2',
       description='de novo protein design through equivariantly diffusing oriented residue clouds',
-      packages=find_namespace_packages(where="genie"),#['genie'],
-      package_dir = {"":"genie"},
+      #packages=find_namespace_packages(where="genie"),#['genie'],
       install_requires=[
             'tqdm',
             'numpy',
