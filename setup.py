7d8556c4f8c569d81838a8e35ec6a3dcad2823cb
Tony <tonyculos@gmail.com>
2024-06-18 13:24:42 -0400

    init files

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
       name='genie',
-      version='0.0.1',
+      version='0.0.2',
       description='de novo protein design through equivariantly diffusing oriented residue clouds',
-      packages=find_packages(),#['genie'],
+      packages=find_packages(where="genie",
+                             include=["*"]),#['genie'],
       install_requires=[
             'tqdm',
             'numpy',
