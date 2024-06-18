54820d33a5787f1a39bc4645a142f284cc62c2f9
Tony <tonyculos@gmail.com>
2024-06-18 13:13:06 -0400

    init files

--- genie/__init__.py
+++ genie/__init__.py
@@ -1,4 +1,7 @@
-from model import *
-from diffusion import *
-from sampler import *
-from utils import *
+from . import model
+from . import diffusion
+from . import sampler
+from . import utils
+from . import constants
+
+__all__ = ["model", "utils", "diffusion", "sampler", "constants"]
